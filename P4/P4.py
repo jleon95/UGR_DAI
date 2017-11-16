78b5cbec0b6ef8a4040005bc720ee0dae8ab588f
Javi <bigfirelp@gmail.com>
2017-11-16 10:37:37 +0100

    Minor fixes

--- P4/P4.py
+++ P4/P4.py
@@ -3,6 +3,7 @@
 from flask import Flask, request, render_template, session, redirect, url_for
 import shelve
 import pymongo as pym
+import urllib as url
 app = Flask(__name__)
 
 app.secret_key = "whatever"
@@ -106,41 +107,22 @@ def ai():
         else:
             return render_template('ai.html', username=None)
 
-# {'_id': ObjectId('5a01e121f516a44e1875e163'),
-# 'address': {'street': '1St Ave', 'coord': [-73.985174, 40.727336], 'building': '130', 'zipcode': '10009'},
-# 'cuisine': 'Bakery',
-# 'grades': [{'date': datetime.datetime(2015, 1, 20, 0, 0), 'grade': 'Not Yet Graded', 'score': 0}],
-# 'restaurant_id': '50018399',
-# 'borough': 'Manhattan',
-# 'name': 'Sweet  Generation'}
-
-@app.route('/restaurant_search', methods = ['GET', 'POST'])
+@app.route('/restaurant_search', methods = ['GET'])
 def restaurant_search():
     restaurants = []
-    if request.method == 'POST':
-        field = request.form['field_name']
-        keywords = request.form['keywords']
+    field = request.args.get('field_name',type=str)
+    keywords = request.args.get('keywords',type=str)
+    if not field is None:
+        keywords = url.parse.unquote_plus(keywords)
         client = pym.MongoClient()
         collection = client.database.restaurants
-        for r in collection.find({field: keywords}):
-            item = "{}. {} {}, {} {}".format(r['name'],r['address']['building'],\
-                    r['address']['street'],r['borough'],r['address']['zipcode'])
-            restaurants.append(item)
-        if 'user' in session:
-            return render_template('restaurant_search.html', username=session['user'],\
-                restaurants=restaurants)
-        else:
-            return render_template('restaurant_search.html', username=None,\
-                restaurants=restaurants)
+        restaurants = list(collection.find({field: keywords}))
+    if 'user' in session:
+        return render_template('restaurant_search.html', username=session['user'],\
+            restaurants=restaurants)
     else:
-        if 'user' in session:
-            return render_template('restaurant_search.html', username=session['user'],\
-                restaurants=restaurants)
-        else:
-            return render_template('restaurant_search.html', username=None,\
-                restaurants=restaurants)
-
-    return render_template('restaurant_search.html', username=session['user'])
+        return render_template('restaurant_search.html', username=None,\
+            restaurants=restaurants)
 
 @app.errorhandler(404)
 def page_not_found(error):
