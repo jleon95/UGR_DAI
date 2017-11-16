28a4c0823ea79cae934234bc4074d400ec350842
Javi <bigfirelp@gmail.com>
2017-11-16 10:39:26 +0100

    Finished P5

--- P5/P5.py
+++ P5/P5.py
@@ -4,6 +4,7 @@ from flask import Flask, request, render_template, session, redirect, url_for
 import shelve
 import pymongo as pym
 import json
+import urllib as url
 app = Flask(__name__)
 
 app.secret_key = "whatever"
@@ -107,40 +108,18 @@ def ai():
         else:
             return render_template('ai.html', username=None)
 
-# {'_id': ObjectId('5a01e121f516a44e1875e163'),
-# 'address': {'street': '1St Ave', 'coord': [-73.985174, 40.727336], 'building': '130', 'zipcode': '10009'},
-# 'cuisine': 'Bakery',
-# 'grades': [{'date': datetime.datetime(2015, 1, 20, 0, 0), 'grade': 'Not Yet Graded', 'score': 0}],
-# 'restaurant_id': '50018399',
-# 'borough': 'Manhattan',
-# 'name': 'Sweet  Generation'}
-
 @app.route('/restaurant_search', methods = ['GET'])
 def restaurant_search():
-    restaurants = []
-    field = request.args.get('field_name',type=str)
-    keywords = request.args.get('keywords',type=str)
-    if not field is None:
-        client = pym.MongoClient()
-        collection = client.database.restaurants
-        restaurants = list(collection.find({field: keywords},{'address': 1, \
-                'cuisine': 1, 'borough': 1, 'name': 1, '_id': 0}))[:20]
     if 'user' in session:
-        return render_template('restaurant_search.html', username=session['user'],\
-            restaurants=restaurants)
+        return render_template('restaurant_search.html', username=session['user'])
     else:
-        return render_template('restaurant_search.html', username=None,\
-            restaurants=restaurants)
-
-    return render_template('restaurant_search.html', username=session['user'])
+        return render_template('restaurant_search.html', username=None)
 
 @app.route('/get_page')
 def get_page():
     field = request.args.get('field',type=str)
     keywords = request.args.get('keywords',type=str)
-    if "+" in keywords:
-        keywords = keywords.split("+")
-        keywords = " ".join(keywords)
+    keywords = url.parse.unquote_plus(keywords)
     number = request.args.get('number',type=int)
     client = pym.MongoClient()
     collection = client.database.restaurants
